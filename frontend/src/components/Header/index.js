fbe104986b7923a901ffca3718624827116f9f66
lmbernardo7520112 <lmbernardo752011@gmail.com>
2020-11-25 19:26:44 -0300

    Adicionado props como children.

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <>
+    
+    <p>Header: {props.title}</p>
+        <p>{props.children}</p>
+        </>
+    )
+}
+
+export default Header;
--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import Header from '../../components/Header';
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -11,6 +11,7 @@ class HomePage extends React.Component {
     render() {
         return (
             <Container>
+                <Header title="título">Header Customizado</Header>
                <FontAwesomeIcon icon='check-square' /> Pitu
             </Container>
         )
