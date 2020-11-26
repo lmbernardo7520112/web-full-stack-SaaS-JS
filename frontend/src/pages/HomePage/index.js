efb1cb8b04007e2dd1c04f32d4f7fe66bd029905
lmbernardo7520112 <lmbernardo752011@gmail.com>
2020-11-26 07:32:05 -0300

    Adicionado Container e Button.

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from '../../components/Header';
+import { Container, InputGroup, FormControl, Button, Alert } from 'react-bootstrap';
+import { ContentContainer, Form } from './styles';
+
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.setState = {
+            isLoading: false,
+            url:'',
+            shortenedURL: '',
+            errorMessage: '',
+        }
     }
 
     render() {
         return (
             <Container>
                 <Header>Seu novo encurtador de URL. :)</Header>
-               <FontAwesomeIcon icon='check-square' /> Pitu
+                <ContentContainer>
+                    <Form>
+                        <InputGroup>
+                        <FormControl placeholder="Digite a URL para encurtar"
+                        defaultValue=""
+                        onChange= {e => this.setState({ url: e.target.value })}
+                        />
+                        <InputGroup.Append>
+                        <Button variant="primary" type="submit">Encurtar</Button>
+                        </InputGroup.Append>
+                        </InputGroup>
+
+                    </Form>
+                </ContentContainer>
             </Container>
         )
     }
